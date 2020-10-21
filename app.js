2563c6b8dfc4a31b8e66a8e153b9a3e19e522d8b
Austin Burger <aburger91@gmail.com>
2020-10-21 11:09:59 -0500

    added conversation dialog

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { Botkit } = require('botkit');
+const { Botkit, BotkitConversation } = require('botkit');
 const { WebAdapter } = require('botbuilder-adapter-web');
 
 
@@ -8,7 +8,37 @@ const controller = new Botkit({
   adapter,
 })
 
+const DIALOG_ID = 'test_dialog_1'
+const dialog = new BotkitConversation(DIALOG_ID, controller)
+dialog.say('well then,')
+dialog.ask('Give me your name.', async (ans) => {
+  dialog.say(`ugh, ${ans}, I can already tell you're going to be worthless`)
+}, { key: 'name' })
+dialog.ask('Are you suggesting that coconuts migrate?', [
+  {
+    pattern: 'yes',
+    handler: async function (ans, convo, bot) {
+      await convo.gotoThread('yes')
+    }
+  },
+  {
+    pattern: 'no',
+    handler: async function (ans, convo, bot) {
+      await convo.gotoThread('no')
+    }
+  }
+], { key: 'coconut' })
+dialog.addMessage('I dont know to say here, maybe you are just fucking stupid m8', 'yes')
+dialog.addMessage('nice', 'no')
+
+dialog.after(async (results, bot) => {
+  const name = results.name;
+});
+
+controller.addDialog(dialog)
+
 controller.on('message', async (bot, msg) => {
-  console.log(msg)
-  await bot.reply(msg, msg.text)
+  if (msg.text === 'ok bot') {
+    await bot.beginDialog('test_dialog_1')
+  }
 })
